c8713a6d9a7ed1993ba6b555f75f7cd8cc1c7f36
dushiyu1 <dushiyu123@icloud.com>
2025-12-13 23:04:24 +0800

    q

--- testcase/test_01.py
+++ testcase/test_01.py
@@ -7,7 +7,7 @@ class Test_01():
 
     with allure.step("请求"):
         def test_001(self):
-            response = requests.get('http://192.128.3.21:8000/api/red-list')
+            response = requests.get('http://localhost:8000/api/red-list')
             print(response.text)
             assert response
 
